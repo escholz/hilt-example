102dc47ba91462437a9844a9eabe4260553b1c67
escholz <eric.scholz@gmail.com>
2020-06-11 21:45:36 -0700

    Initial Import
    - Activity/Fragment/ViewModel injection with Hilt

---
+++ app/src/main/java/escholz/examples/hilt/MainActivity.kt
@@ -0,0 +1,23 @@
+package escholz.examples.hilt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import escholz.examples.hilt.ui.main.MainFragment
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.main_activity)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    // TODO: remove newInstance()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/escholz/examples/hilt/ui/main/MainViewModel.kt
@@ -0,0 +1,16 @@
+package escholz.examples.hilt.ui.main
+
+import android.app.Application
+import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+class MainViewModel
+@ViewModelInject
+constructor(
+    private val application: Application,
+    @Assisted private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    // TODO: Implement the ViewModel
+}
